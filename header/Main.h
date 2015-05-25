8cebb5d5d08768b0222cd1386c6acb16c0ce0e13
Time-bomb <nicolas.bedrine@telecomnancy.net>
2015-05-25 20:53:08 +0200

    last commit and last comment

--- header/Main.h
+++ header/Main.h
@@ -17,14 +17,27 @@
 
 int main();
 int managing_event(SDL_Window * main_screen, SDL_Renderer *rendu);
+// Fonction qui gere les differents evenements du programme, notamment l'ecran du jeu lui-meme avec la gestion des touches, des differents sprites et les differents ecran
+
 int managing_keybinds(SDL_Window * main_screen, SDL_Renderer *rendu);
+// Gestion de la configuration des touches. Le joueur peut choisir grâce à un ecran de selection les differents touches qu'il souhaite assigner à un evenement
+
 void init_texture(SDL_Renderer *rendu, SDL_Texture *tableau[]);
 void init_texture_attack(SDL_Renderer *rendu, SDL_Texture *tabl[]);
 void init_type_attack(SDL_Renderer *rendu, SDL_Texture *tableau[]);
+// Ces fonctions chargent au début de la fonction managing_event les differents sprites necessaires au jeu. Ils n'ont donc plus besoin d'etre recharge ensuite.
+
 void destroy_texture(int taille, SDL_Texture *tableau[]);
+//Cette fonction libere la place des differents textures pour eviter notamment les fuites memoires.
+
 void compute_tram(int *j, int *trame);
+//Cette fonction calcule la trame actuelle pour donner une impression de mouvement des membres lors du deplacement d'un personnage
+
 void changeTypeSpell (int key[], TypeSpell **tab_typeSpell, TypeSpell **current_type);
+//Cette fonction change le type de sort courant selon le type de sort selectionne
+
 void init_texture_ennemy(SDL_Renderer *rendu, SDL_Texture *tabl[]);
+//Charge en debut de partie les sprites pour les monstres
 void deleteSpell2(ListSpell *current_list, Map *map, ListItem *listitem);
 void hero_stat(SDL_Renderer *rendu, TTF_Font* font, Map *map);
 
--- header/Spell.h
+++ header/Spell.h
@@ -36,16 +36,28 @@ struct ListSpell{
 	struct ListSpell* nextSpell;
 };
 
+//Tous les sorts sont stockes dans un liste chainee pour faciliter leur "collage sur l'ecran". Chaque sort possède sa position actuelle et sa direction. 
+
 Spell* createSpell(TypeSpell *type, int *direction, int x, int y);
+//Fonction qui alloue un nouveau Spell en memoire
 void deleteSpell(ListSpell *current_list);
+//Fonction qui detruit un spell en memoire tout en laissant intact la structure de la ligne chainee contenant les sorts
 void lanceattack(ListSpell *current_list, int *f, TypeSpell *current_type, int key[], int x, int y);
+//Fonction qui selon la direction actuelle et les touches appuyées ajoute un sort dans la liste chainee des sorts
 TypeSpell *init_typeSpell(int attack, int id, int rate, int ammo);
+//Fonction qui creer un nouveau type de sort
 void Blit_attack(ListSpell *current_list,SDL_Renderer *rendu, SDL_Texture *tableau[]);
+//Fonction qui parcourt une liste chaine contenant les sorts et les colle sur l'ecran
 ListSpell *init_listspell();
+//Fonction qui initialise la liste de sorts lancees
 void update_blitmap (Move* move, SDL_Rect *position);
+//Fonction qui recalcule une position sur l'ecran en fonction du mouvement de la carte
 void manageSpellType(TypeSpell *current_type, SDL_Renderer *rendu, SDL_Texture *texture_type[], TTF_Font* font);
+//Fonction qui gere l'affiche du type de sort actuelle ainsi que l'affiche du nombre de sort restant
 void updateSpell(ListSpell *current_list, Move* move, SDL_Window *main_screen);
+//Fonction qui calcule les mouvements des sorts en fonction de leur direction et des mouvements de la carte
 void destroyListSpell(ListSpell* ls);
 void destroySpell(Spell* s);
+//Fonction qui libere la memoire utilise par la liste et par un sort
 
 #endif
