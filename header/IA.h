f2ffe3d014ed4cbe7921a8df0cdac812ea6c81d2
Neressea <albert.vincent@hotmail.fr>
2015-05-25 21:18:53 +0200

    Adding comments to IA

--- header/IA.h
+++ header/IA.h
@@ -7,10 +7,30 @@
 #include "Map.h"
 #include "Character.h"
 
+/**
+* Manage the number and the diffculty of the ennemies.
+*/
 void manageEnnemies(Map* map);
+
+/**
+* Create a new ennemy on the map. Called by manageEnnemies()
+*/
 void createEnnemy(Map* m, int level);
+
+/**
+* Get the number of ennemies there is on the map.
+*/
 int numberOfEnnemies(Map* map);
+
+/**
+* Move all the ennemies in function of the position of th hero.
+* At the beginning, we wanted to use Dijkstra, but unfortunately we hadn't enough time to.
+*/
 void moveEnnemy(ListBuilding* b, Character* heros, Character *ennemy, int key[]);
+
+/**
+* Deplace a particular ennemy. Check the collisions.
+*/
 void deplaceEnnemy(ListBuilding* b, int key[],Character *ennemy);
 
 #endif
--- src/IA.c
+++ src/IA.c
@@ -21,6 +21,7 @@ void moveEnnemy(ListBuilding* b, Character* heros, Character *ennemy, int key[])
 	dist->x = heros->pos->x - ennemy->pos->x;
 	dist->y = heros->pos->y - ennemy->pos->y;
 
+	//We compute the position of the ennemy in function of the position of the hero.
 	key[0] = (dist->y < 0) ? 1 : 0;
 	key[1] = (dist->y != 0) ? !key[0] : 0;
 	key[2] = (dist->x < 0) ? 1 : 0;
@@ -33,8 +34,11 @@ void moveEnnemy(ListBuilding* b, Character* heros, Character *ennemy, int key[])
 void deplaceEnnemy(ListBuilding *b, int key[], Character *ennemy){
 	SDL_Rect* position = ennemy->pos;
 	int x = position->x, y = position->y;
+
+	//A static variable to memorize the number of time we get in the function. Reinitialized after the tenth call.
 	static int pool = 1; 
 
+	//We change the position of the ennemy
 	if (key[0] && key[2]){
 		position->y -= SPEED / 3;
 		position->x -= SPEED / 3;
@@ -46,7 +50,6 @@ void deplaceEnnemy(ListBuilding *b, int key[], Character *ennemy){
 	if (key[0] && !key[3] && !key[2]){
 		position->y -= SPEED / 3;
 	}
-	
 	if (key[1] && key[2]){
 		position->y += SPEED / 3;
 		position->x -= SPEED / 3;
@@ -67,10 +70,12 @@ void deplaceEnnemy(ListBuilding *b, int key[], Character *ennemy){
 	}
 
 	Building *build;
+	//We check the collision with buildings.
 	if((build = cantMove(b, position))){
 		position->x = x;
 		position->y = y;
 
+		//We don't want the building to be destroyed too easily, so we give damages only all tenth call.
 		if(build->hp > 0 && pool % 10 == 0){
 			build->hp -= ennemy->attack;
 			pool = 1;
@@ -85,14 +90,15 @@ void createEnnemy(Map* map, int level){
 	//We select the position where the ennemy will appear
 	int width = map->width;
 	int height = map->height;
-
 	SDL_Rect *pos = malloc(sizeof(SDL_Rect));
 
+	//We check that the position is free, that's to say there is no building and not the hero.
 	do{
 		pos->x = abs((rand() * PX_W)%width);
 		pos->y = abs((rand() * PX_H)%height);
 	}while(isFree(map, pos));
 
+	//We create and add the ennemy to the map.
 	Character* ch = createChar(100 + level * 20, 100 * (level-1), level, 0, 0, pos->x/PX_W, pos->y/PX_H, 50, 61, NULL);
 	addChar(map->characters, ch);
 
